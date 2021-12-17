9ce77ea206ef76d4f0e3fc083b8712736e38009e
Denis Roussel <denis.roussel@acsone.eu>
2021-12-17 15:44:17 +0100

    [14.0][IMP] base_tier_validation: Add hook for state transition

--- base_tier_validation/models/tier_validation.py
+++ base_tier_validation/models/tier_validation.py
@@ -341,6 +341,15 @@ class TierValidation(models.AbstractModel):
                 allowed_field_names.append(fld_data["string"])
         return allowed_field_names, not_allowed_field_names
 
+    def _check_tier_state_transition(self, vals):
+        """
+        Check we are in origin state and not destination state
+        """
+        self.ensure_one()
+        return getattr(self, self._state_field) in self._state_from and not vals.get(
+            self._state_field
+        ) in (self._state_to + [self._cancel_state])
+
     def write(self, vals):
         self._tier_validation_check_state_on_write(vals)
         self._tier_validation_check_write_allowed(vals)
